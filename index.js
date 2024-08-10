61788ebcb7d07c50d02a3772367026bb9a1e1617
Nick Lombardi <nick@streamforge.gg>
2024-08-09 21:10:35 -0400

    try

--- index.js
+++ index.js
@@ -3,58 +3,59 @@ import contentTypes from './content-types.js'
 import Scraper from './scraper.js'
 import { generateJSONResponse, generateErrorJSONResponse } from './json-response.js'
 
-addEventListener('fetch', (event) => {
-    event.respondWith(handleRequest(event.request))
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
 })
 
 async function handleRequest(request) {
-    const searchParams = new URL(request.url).searchParams
+  const searchParams = new URL(request.url).searchParams
 
-    let url = searchParams.get('url')
-    if (url && !url.match(/^[a-zA-Z]+:\/\//)) url = 'http://' + url
+  let url = searchParams.get('url')
+  if (url && !url.match(/^[a-zA-Z]+:\/\//)) url = 'http://' + url
 
-    const selector = searchParams.get('selector')
-    const attr = searchParams.get('attr')
-    const spaced = searchParams.get('spaced') // Adds spaces between tags
-    const pretty = searchParams.get('pretty')
+  const selector = searchParams.get('selector')
+  const attr = searchParams.get('attr')
+  const spaced = searchParams.get('spaced') === 'true'
+  const pretty = searchParams.get('pretty') === 'true'
 
-    if (!url || !selector) {
-        return handleSiteRequest(request)
-    }
+  if (!url || !selector) {
+    return handleSiteRequest(request)
+  }
 
-    return handleAPIRequest({ url, selector, attr, spaced, pretty })
+  return handleAPIRequest({ url, selector, attr, spaced, pretty })
 }
 
 async function handleSiteRequest(request) {
-    const url = new URL(request.url)
+  const url = new URL(request.url)
 
-    if (url.pathname === '/' || url.pathname === '') {
-        return new Response(html, {
-            headers: { 'content-type': contentTypes.html },
-        })
-    }
+  if (url.pathname === '/' || url.pathname === '') {
+    return new Response(html, {
+      headers: { 'content-type': contentTypes.html }
+    })
+  }
 
-    return new Response('Not found', { status: 404 })
+  return new Response('Not found', { status: 404 })
 }
 
 async function handleAPIRequest({ url, selector, attr, spaced, pretty }) {
-    let scraper, result
+  let scraper, result
 
-    try {
-        scraper = await new Scraper().fetch(url)
-    } catch (error) {
-        return generateErrorJSONResponse(error, pretty)
+  try {
+    scraper = await new Scraper().fetch(url)
+  } catch (error) {
+    return generateErrorJSONResponse(error, pretty)
+  }
+
+  try {
+    if (!attr) {
+      result = await scraper.querySelector(selector).getText({ spaced })
+    } else {
+      const attributes = attr.split(',').map(a => a.trim())
+      result = await scraper.querySelector(selector).getAttributes(attributes)
     }
+  } catch (error) {
+    return generateErrorJSONResponse(error, pretty)
+  }
 
-    try {
-        if (!attr) {
-            result = await scraper.querySelector(selector).getText({ spaced })
-        } else {
-            result = await scraper.querySelector(selector).getAttribute(attr)
-        }
-    } catch (error) {
-        return generateErrorJSONResponse(error, pretty)
-    }
-
-    return generateJSONResponse({ result }, pretty)
+  return generateJSONResponse({ result }, pretty)
 }
--- scraper.js
+++ scraper.js
@@ -1,121 +1,112 @@
-const cleanText = (s) => s.trim().replace(/\s\s+/g, ' ')
+const cleanText = s => s.trim().replace(/\s\s+/g, ' ')
 
 class Scraper {
-    constructor() {
-        this.rewriter = new HTMLRewriter()
-        return this
+  constructor() {
+    this.rewriter = new HTMLRewriter()
+    return this
+  }
+
+  async fetch(url) {
+    this.url = url
+    this.response = await fetch(url)
+
+    const server = this.response.headers.get('server')
+
+    const isThisWorkerErrorNotErrorWithinScrapedSite = (
+      [530, 503, 502, 403, 400].includes(this.response.status) &&
+      (server === 'cloudflare' || !server /* Workers preview editor */)
+    )
+
+    if (isThisWorkerErrorNotErrorWithinScrapedSite) {
+      throw new Error(`Status ${this.response.status} requesting ${url}`)
     }
 
-    async fetch(url) {
-        this.url = url
-        this.response = await fetch(url)
+    return this
+  }
 
-        const server = this.response.headers.get('server')
+  querySelector(selector) {
+    this.selector = selector
+    return this
+  }
 
-        const isThisWorkerErrorNotErrorWithinScrapedSite =
-            [530, 503, 502, 403, 400].includes(this.response.status) &&
-            (server === 'cloudflare' || !server) /* Workers preview editor */
+  async getText({ spaced }) {
+    const matches = {}
+    const selectors = this.selector.split(',').map(s => s.trim())
 
-        if (isThisWorkerErrorNotErrorWithinScrapedSite) {
-            throw new Error(`Status ${this.response.status} requesting ${url}`)
+    selectors.forEach((selector) => {
+      matches[selector] = []
+
+      let nextText = ''
+
+      this.rewriter.on(selector, {
+        element(element) {
+          matches[selector].push(true)
+          nextText = ''
+        },
+
+        text(text) {
+          nextText += text.text
+
+          if (text.lastInTextNode) {
+            if (spaced) nextText += ' '
+            matches[selector].push(nextText)
+            nextText = ''
+          }
         }
+      })
+    })
 
-        return this
-    }
+    const transformed = this.rewriter.transform(this.response)
 
-    querySelector(selector) {
-        this.selector = selector
-        return this
-    }
+    await transformed.arrayBuffer()
 
-    async getText({ spaced }) {
-        const matches = {}
-        const selectors = new Set(this.selector.split(',').map((s) => s.trim()))
+    selectors.forEach((selector) => {
+      const nodeCompleteTexts = []
 
-        selectors.forEach((selector) => {
-            matches[selector] = []
+      let nextText = ''
 
-            let nextText = ''
-
-            this.rewriter.on(selector, {
-                element(element) {
-                    matches[selector].push(true)
-                    nextText = ''
-                },
-
-                text(text) {
-                    nextText += text.text
-
-                    if (text.lastInTextNode) {
-                        if (spaced) nextText += ' '
-                        matches[selector].push(nextText)
-                        nextText = ''
-                    }
-                },
-            })
-        })
-
-        const transformed = this.rewriter.transform(this.response)
-
-        await transformed.arrayBuffer()
-
-        selectors.forEach((selector) => {
-            const nodeCompleteTexts = []
-
-            let nextText = ''
-
-            matches[selector].forEach((text) => {
-                if (text === true) {
-                    if (nextText.trim() !== '') {
-                        nodeCompleteTexts.push(cleanText(nextText))
-                        nextText = ''
-                    }
-                } else {
-                    nextText += text
-                }
-            })
-
-            const lastText = cleanText(nextText)
-            if (lastText !== '') nodeCompleteTexts.push(lastText)
-            matches[selector] = nodeCompleteTexts
-        })
-
-        return selectors.length === 1 ? matches[selectors[0]] : matches
-    }
-
-    async getAttributes(attributes) {
-        const matches = {}
-        const selectors = new Set(this.selector.split(',').map((s) => s.trim()))
-
-        class AttributeScraper {
-            constructor(attrs) {
-                this.attrs = Array.isArray(attrs) ? attrs : [attrs]
-                this.results = []
-            }
-
-            element(element) {
-                const attrValues = {}
-                this.attrs.forEach((attr) => {
-                    attrValues[attr] = element.getAttribute(attr)
-                })
-                this.results.push(attrValues)
-            }
+      matches[selector].forEach(text => {
+        if (text === true) {
+          if (nextText.trim() !== '') {
+            nodeCompleteTexts.push(cleanText(nextText))
+            nextText = ''
+          }
+        } else {
+          nextText += text
         }
+      })
 
-        selectors.forEach((selector) => {
-            const scraper = new AttributeScraper(attributes)
-            this.rewriter.on(selector, scraper)
-            matches[selector] = scraper
-        })
+      const lastText = cleanText(nextText)
+      if (lastText !== '') nodeCompleteTexts.push(lastText)
+      matches[selector] = nodeCompleteTexts
+    })
 
-        await this.rewriter.transform(this.response).arrayBuffer()
+    return selectors.length === 1 ? matches[selectors[0]] : matches
+  }
 
-        selectors.forEach((selector) => {
-            matches[selector] = matches[selector].results
-        })
+  async getAttributes(attributes) {
+    const matches = {}
+    const selectors = this.selector.split(',').map(s => s.trim())
+    const attrs = Array.isArray(attributes) ? attributes : [attributes]
 
-        return selectors.length === 1 ? matches[selectors[0]] : matches
-    }
+    selectors.forEach((selector) => {
+      matches[selector] = []
+
+      this.rewriter.on(selector, {
+        element(element) {
+          const attrValues = {}
+          attrs.forEach(attr => {
+            attrValues[attr] = element.getAttribute(attr)
+          })
+          matches[selector].push(attrValues)
+        }
+      })
+    })
+
+    await this.rewriter.transform(this.response).arrayBuffer()
+
+    return selectors.length === 1 ? matches[selectors[0]] : matches
+  }
 }
 
 export default Scraper
